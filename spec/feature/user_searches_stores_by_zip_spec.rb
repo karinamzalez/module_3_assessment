fa743e682380588e8fa95db7cbc90040d8953110
Karina Gonzalez <karinamzalez@gmail.com>
2016-09-20 10:44:17 -0600

    add search form on root page

---
+++ spec/feature/user_searches_stores_by_zip_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "guest user displays items" do
+  scenario "user displays items" do
+  visit '/'
+
+  within(:search_box) do
+    fill_in :zip, with: "80202"
+  end
+
+  click_on "search"
+
+  expect(current_path).to include '/search'
+
+  within('#store_list') do
+    expect(page).to have_content "17 Total Stores"
+    expect(stores.count).to eq 15
+    expect(stores.first.long_name).to eq ""
+    expect(stores.first.city).to eq ""
+    expect(stores.first.distance).to eq ""
+    expect(stores.first.phone_number).to eq ""
+    expect(stores.first.store_type).to eq ""
+  end
+# And I should see stores within 25 miles of 80202
+# And I should see a message that says "17 Total Stores"
+# And I should see exactly 15 results
+# And I should see the long name, city, distance, phone number and store type for each of the 15 results
+  end
+end
