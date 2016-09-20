1d144f26ba6eeeb926390f3fa141010950d398ca
Karina Gonzalez <karinamzalez@gmail.com>
2016-09-20 09:13:37 -0600

    add first feature test -- user get json parsed list of items * add 'api/v1/items' route * add 'api/v1/items' view

--- spec/feature/user_displays_items_spec.rb
+++ spec/feature/user_displays_items_spec.rb
@@ -9,4 +9,11 @@ RSpec.feature "guest user displays items" do
       expect(page).to have_content('Items'), "page should have an Items heading"
     end
   end
+
+  scenario "user gets all items" do
+    visit '/api/v1/items'
+    expect(page.status_code).to eq(200)
+
+    
+  end
 end
